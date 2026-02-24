316e3a952148f218424d344fe2edf7f60b2e6187
swoo <kokodd1234@gmail.com>
2026-02-24 09:42:44 +0900

    [Init] 프로젝트 초기설정

---
+++ src/plugins/axiosinterceptor.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+// URL 설정
+const api = axios.create({
+  baseURL: 'http://localhost:5173/api',
+  timeout: 5000,
+})
+
+// 요청
+api.interceptors.request.use(
+  (config) => {
+    console.log('요청 보내기 전에 실행')
+    return config
+  },
+  () => {
+    // 에러 발생시 코드
+    console.log('요청 보낼 때 에러 발생')
+    return Promise.reject(error)
+  },
+)
+
+// 응답
+api.interceptors.response.use(
+  (config) => {
+    console.log('응답 받아서 화면에 띄우기 전에 실행')
+    return config
+  },
+  () => {
+    // 에러 발생시 코드
+    console.log('응답 받을 때 에러 발생')
+    return Promise.reject(error)
+  },
+)
+
+export default api
